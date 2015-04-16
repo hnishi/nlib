5f6205df90b2e2703b2ab55ff2d6ae2d2baeb3be
hnishi <nishigami2509@gmail.com>
2015-04-16 15:50:17 +0900

    add math_nishi.cpp

--- math_nishi.h
+++ math_nishi.h
@@ -1,4 +1,4 @@
-//#include"nlib.h"
+#include"nlib.h"
 #include<cmath>
 #include<vector>
 using namespace std;
@@ -17,22 +17,12 @@ using namespace std;
 
 #ifndef _INCLUDE_RMSD_
 #define _INCLUDE_RMSD_
-inline double rmsd(vector<double> &ax,vector<double> &ay,vector<double> &az,
-		vector<double> &bx,vector<double> &by,vector<double> &bz) // to calculate R.M.S.D.
-{
-	double rmsd,tot_x=0,tot_y=0,tot_z=0;
-	//double sum=0;
-	if(ax.size() != bx.size()){
-		cerr<<"ERROR: both structures should have the same num. of atoms\n";
-		return -1;
-	}
-	for(unsigned int u=0;u<ax.size();u++){
-		tot_x += ( ax[u]- bx[u])*( ax[u]- bx[u]);
-		tot_y += ( ay[u]- by[u])*( ay[u]- by[u]);
-		tot_z += ( az[u]- bz[u])*( az[u]- bz[u]);
-	}
-	//sum = tot_x + tot_y + tot_z;
-	rmsd=sqrt((tot_x + tot_y + tot_z)/ ax.size());
-        return rmsd;
-}
+//inline double rmsd(vector<double> &ax,vector<double> &ay,vector<double> &az,
+double rmsd(vector<double> &ax,vector<double> &ay,vector<double> &az,
+		vector<double> &bx,vector<double> &by,vector<double> &bz); // to calculate R.M.S.D.
+#endif
+
+#ifndef _INCLUDE_RMSD_2_
+#define _INCLUDE_RMSD_2_
+double rmsd2(vector<double> &a,vector<double> &b); // to calculate R.M.S.D.
 #endif
